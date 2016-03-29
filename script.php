6a7d08533e906d4a2d2ce9071ab7208141ea9476
Okina1Raion <sany_z@mail.ua>
2016-03-29 10:24:33 +0300

    Update script.php
    shl Work

--- script.php
+++ script.php
@@ -1,4 +1,6 @@
 <?
+$user=$_POST['name'];
+if(isset($_POST['submit'])){
 $hostname = "localhost"; 
 $username = "root"; 
 $password = ""; 
@@ -8,5 +10,9 @@ $userstable = "123";
 mysql_connect($hostname,$username,$password) OR DIE("Не могу создать соединение "); 
 mysql_select_db($dbName) or die(mysql_error()); 
 $query = "INSERT INTO $userstable VALUES('$name')";
-mysql_query($query) or die(mysql_error()); 
+mysql_query($query) or die(mysql_error());
+$q2 =mysql_query("SELECT * users WHERE login='$user'");
+$arr= mysql_fetch_array($q2);
+  echo $arr['login']." Hi";
+}
 ?>
